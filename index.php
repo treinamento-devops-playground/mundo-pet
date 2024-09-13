e9253770a6c40b1c426c1e17b7c684a248c27caa
SidyDev-AI <sidydev.io@gmail.com>
2024-09-13 20:29:35 -0300

    Add file cadastro.php and cadastro.css

--- index.php
+++ index.php
@@ -5,19 +5,19 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Document</title>
-  <link rel="stylesheet" href="/style.css">
+  <link rel="stylesheet" href="./app/css/index.css">
 </head>
 
 <body>
   <nav id="nav-bar">
     <div class="logo">
-      <img src="./src/img/logo.png" alt="Logo Mundo Pet">
+      <img src="./app/img/logo.png" alt="Logo Mundo Pet">
     </div>
     <ul class="list-nav">
       <li class="title"><a href="#">Serviços</a></li>
       <li class="title"><a href="#">Loja</a></li>
       <li class="title"><a href="#">Contato</a></li>
-      <li><a href="#"><img src="./src/img/icons/user.png"></a></li>
+      <li><a href="#"><img src="./app/img/icons/user.png"></a></li>
     </ul>
   </nav>
 
@@ -25,11 +25,11 @@
     <p class="slogan">
       Mundo Pet!</br>
       O melhor para o seu pet,</br>
-      em um clique!!! <img src="./src/img/icons/click.png">
+      em um clique!!! <img src="./app/img/icons/click.png">
     </p>
     <div class="buttons">
-      <button class="btn btn-primary"><a href="./src/pages/login.php">Login</a></button>
-      <button class="btn btn-second"><a href="./src/pages/cadastro.php">Cadastrar</a></button>
+      <button class="btn btn-primary"><a href="./app/pages/login.php">Login</a></button>
+      <button class="btn btn-second"><a href="./app/pages/cadastro.php">Cadastrar</a></button>
     </div>
   </div>
 </body>
