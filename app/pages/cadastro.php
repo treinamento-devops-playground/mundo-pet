bfa5279a0d28563d893ae9549b1e66d63cc9b8f6
Andre_victor <andre_victor.a@hotmail.com>
2024-10-04 19:30:43 -0300

    cadastro de usuario

--- app/pages/cadastro.php
+++ app/pages/cadastro.php
@@ -10,23 +10,25 @@
   <div id="container">
     <img src="../img/logo.png" alt="Logo Mundo Pet" class="logo">
     <h2>Cadastrar</h2>
+    <form action="../api/cadastro_user.php" method="POST">
     <div class="input-container">
       <img src="../img/icons/user-fill.png">
-      <input type="text" placeholder="Nome">
+      <input type="text" placeholder="Nome" id="username" name="username" required>
     </div>
     <div class="input-container">
       <img src="../img/icons/email.png">
-      <input type="email" placeholder="Email">
+      <input type="email" placeholder="Email" id="email" name="email" required>
     </div>
     <div class="input-container">
       <img src="../img/icons/padlock.png">
-      <input type="password" placeholder="Senha">
+      <input type="password" placeholder="Senha" id="password" name="password" required>
     </div>
     <div class="input-container">
       <img src="../img/icons/phone-solid.png">
-      <input type="number_format" placeholder="Celular">
+      <input type="number_format" placeholder="Celular" id="telefone" name="telefone" required>
     </div>
     <button class="btn">Cadastrar</button>
+    </form>
     <div class="social-login">
       <a href="#"><img src="../img/icons/google.png" alt="Logo google"></a>
       <a href="#"><img src="../img/icons/facebook.png" alt="Logo facebook"></a>
