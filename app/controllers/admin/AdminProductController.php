d73b6d2844f8db876a575a6391434227ec9cfb96
henrique-noronha <henriquenoronha020@gmail.com>
2024-10-22 23:06:29 -0300

    .

--- app/controllers/admin/AdminProductController.php
+++ app/controllers/admin/AdminProductController.php
@@ -20,14 +20,14 @@ class AdminProductController extends BaseController
 
     public function store()
     {
-        $name = $_POST['nome'];
-        $description = $_POST['descricao'];
-        $price = $_POST['preco'];
+        $name = $_POST['name'];
+        $description = $_POST['description'];
+        $price = $_POST['price'];
         $info = $_POST['info'];
-        $category = $_POST['categoria'];
-        $estoque = $_POST['estoque'];
+        $category = $_POST['category'];
+        $stock = $_POST['stock'];
 
-        $success = ProductModel::create($name, $description, $price, $info, $category, $estoque);
+        $success = ProductModel::create($name, $description, $price, $info, $category, $stock);
 
         if ($success) {
             header('Location: /admin/products?success=Produto cadastrado com sucesso');
